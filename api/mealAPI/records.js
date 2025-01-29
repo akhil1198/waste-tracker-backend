b065597b2d76e238418af192421a65be70991068
akhil1198 <akhilshridhar02@gmail.com>
2025-01-29 01:26:10 -0500

    api updates

--- api/mealAPI/records.js
+++ api/mealAPI/records.js
@@ -11,18 +11,7 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
-        const params = {
-            TableName: 'mealwastetracker',
-            ScanIndexForward: false,
-        };
 
-        try {
-            const data = await dynamoDB.scan(params).promise();
-            res.status(200).send("hello from records");
-        } catch (err) {
-            console.error('Error fetching records:', err);
-            res.status(500).json({ error: 'Failed to fetch records' });
-        }
     } else {
         res.status(405).json({ error: 'Method Not Allowed' });
     }
--- api/mealAPI/submit-order.js
+++ api/mealAPI/submit-order.js
@@ -11,33 +11,7 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const { id, mealType, quantity, date } = req.body.data;
 
-        if (!mealType || !quantity || !date) {
-            res.status(400).json({ error: 'All fields are required!' });
-            return;
-        }
-
-        const data = {
-            mealType,
-            quantity,
-            date,
-            id,
-        };
-
-        const params = {
-            TableName: 'mealwastetracker',
-            Item: data,
-        };
-
-        try {
-            await dynamoDB.put(params).promise();
-            console.log('Data inserted in DynamoDB', data);
-            res.status(200).json({ message: 'Data inserted successfully', data: params.Item });
-        } catch (error) {
-            console.error('Error updating DynamoDB:', error);
-            res.status(500).json({ error: 'Failed to insert data' });
-        }
     } else {
         res.status(405).json({ error: 'Method Not Allowed' });
     }
